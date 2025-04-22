ccbe6af1c73ca8d3cb0aa7193f73f95c47c9942d
11PRIA <priyanka11062003@gmail.com>
2025-04-22 08:14:32 +0530

    Update index.js for Render deployment and package.json scripts

--- server/index.js
+++ server/index.js
@@ -1,35 +1,53 @@
-const {Server} =require("socket.io")
-const io=new Server(8000,{
-    cors:true,
-})
-const emailToSocketIdMap=new Map();
-const socketIdToEmailMap=new Map();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
+const app = express();
+app.use(cors()); // Optional but useful
 
-io.on("connection",(socket)=>{
-console.log(`Socket connected`,socket.id)
-socket.on("room:Join",(data)=>{
-    const {email,room}=data;
-    emailToSocketIdMap.set(email,socket.id);
-    socketIdToEmailMap.set(socket.id,email);
-    socket.join(room)
-    io.to(room).emit("user:joined",{email,id:socket.id})
-    io.to(socket.id).emit("room:Join",data)
-})
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // You can restrict this in production
+    methods: ["GET", "POST"]
+  }
+});
 
-socket.on("user:call",({to,offer})=>{
-    io.to(to).emit("incoming:call",{from:socket.id,offer})
-})
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
 
-socket.on("call:accepted",({to,ans})=>{
-    io.to(to).emit("call:accepted",{from:socket.id,ans})
-})
+io.on("connection", (socket) => {
+  console.log(`Socket connected:`, socket.id);
 
-socket.on("peer:nego:needed",({to,offer})=>{
-    io.to(to).emit("peer:nego:needed",{from:socket.id,offer})
-})
+  socket.on("room:Join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email);
+    socket.join(room);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    io.to(socket.id).emit("room:Join", data);
+  });
 
-socket.on("peer:nego:done",({to,ans})=>{
-    io.to(to).emit("peer:nego:final",{from:socket.id,ans})
-})
-})
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incoming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
+
+// Render will set the PORT for you
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
